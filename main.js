e78e2d929ade70dec3328eef81b1ab530193712c
Kyryl5 <kirillsavitsky56@gmail.com>
2022-08-15 14:49:41 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let reg = document.getElementById('register');
+let log = document.getElementById('login');
+let btn = document.getElementById('btn');
+        
+        function login() {
+            reg.style.left = '450px';
+            log.style.left = '50px';
+            btn.style.left = '0px';
+        }
+        function register() {
+            reg.style.left = '50px';
+            log.style.left = '-400px';
+            btn.style.left = '110px';
+        }
+      
